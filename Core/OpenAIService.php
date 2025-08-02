d5c5a1d310c0b28db229085c400e88d27213dbbc
salarshirkhani <salarshirkhany16@gmail.com>
2025-08-02 17:28:28 +0330

    resolve git push

--- Core/OpenAIService.php
+++ Core/OpenAIService.php
@@ -5,7 +5,7 @@ use OpenAI;
 
 class OpenAIService {
     public static function client() {
-        $apiKey = '';
+        $apiKey = dotenv('apikey');
         if (!$apiKey) {
             throw new \Exception('OpenAI API key is missing. Please set OPENAI_API_KEY in .env file.');
         }
